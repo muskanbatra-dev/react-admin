e58d35486f35db62132fc34962a2184484adbc29
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2022-07-07 12:39:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import {Admin , Resource} from 'react-admin';
-
-const App = () => {
+import {
+  Admin,
+  Resource,
+  
+} from "react-admin";
+import jsonServerProvider from 'ra-data-json-server';
+import {
+  UserList,
+  UserEdit,
+  UserCreate,
+} from './Users';
+import authProvider from "./authProvider";
+ 
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+ 
+function App() {
   return (
-    <div>App</div>
-  )
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <Resource
+        name="users"
+        list={UserList}
+        edit={UserEdit}
+        create={UserCreate}
+      />
+    
+<Admin dataProvider={dataProvider}></Admin>
+<Resource
+        name="superadmin"
+        list={UserList}
+        edit={UserEdit}
+        create={UserCreate}
+      />
+    </Admin>
+  );
 }
-
-export default App
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,55 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    
+    
+    Edit,
+    SimpleForm,
+    TextInput,
+    Create,
+  } from 'react-admin';
+   
+  export const UserList = props => (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        <TextField source="username" />
+       
+        <TextField source="address.street" label="Address" />
+        <TextField source="phone" />
+        
+        <TextField source="company.name" label="Company" />
+      </Datagrid>
+    </List>
+  );
+   
+  export const UserEdit = props => (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="id" disabled />
+        <TextInput source="name" />
+        <TextInput source="username" />
+        
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        
+        <TextInput source="company.name" label="Company" />
+      </SimpleForm>
+    </Edit>
+  );
+   
+  export const UserCreate = props => (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput source="name" />
+        <TextInput source="username" />
+       
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        
+        <TextInput source="company.name" label="Company" />
+      </SimpleForm>
+    </Create>
+  )
